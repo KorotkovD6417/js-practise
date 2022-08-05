2399e57794c2b65ed4ff7540c0278e5fe46b665e
Korotkov Daniil <daniilkorotkov6417@gmail.com>
2022-08-05 21:05:04 +0300

    1

---
+++ js/module-one.js
@@ -0,0 +1,17 @@
+const x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i <= x.length; i += 1) {
+
+    if (i === 8) {
+
+        break;
+    }
+    console.log(i);
+}
+
+const newObject = {
+    book: 1984,
+    author: 'Orwell',
+    genre: 'utopia',
+}
+console.log(newObject);
